de9d653bddc9e6d468187de7dd0623d78ccc14bc
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-13 12:46:50 +0530

    Shooting and Animations

---
+++ Pixel Man 2D/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 1f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        speed = -speed;
+    }
+}
